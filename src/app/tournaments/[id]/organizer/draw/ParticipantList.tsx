87b23232f9cb755b918c00a341532b00f410bffe
Nonnadda <tontongfordev@gmail.com>
2025-09-16 14:51:16 +0300

    draw page

--- src/app/tournaments/[id]/organizer/draw/ParticipantList.tsx
+++ src/app/tournaments/[id]/organizer/draw/ParticipantList.tsx
@@ -1,13 +1,14 @@
 import { MAP_GROUP_NAME, SERVICE_ENDPOINT } from '@/app/constants'
 import { RootState } from '@/app/libs/redux/store'
 import { Event, EventTeam, Language, Player } from '@/type'
-import { Box, Button, Card, CardContent, CardHeader, Divider, FormControl, InputLabel, MenuItem, Paper, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@mui/material'
+import { Box, Button, Card, CardContent, CardHeader, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, FormControl, InputLabel, MenuItem, Paper, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@mui/material'
 import axios from 'axios'
 import { MouseEvent, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
 import PlayerPopover from '../../participants/PlayerPopover'
 import DrawBracket from './drawBracket'
+import Transition from '@/app/components/ModalTransition'
 
 interface ParticipantListProps {
   eventID: string,
@@ -23,6 +24,7 @@ const ParticipantList = ({ eventID }: ParticipantListProps) => {
   const [draw, setDraw] = useState<Event['draw']>({})
   const [numPlayoff, setNumPlayoff] = useState(8)
   const [numGroup, setNumGroup] = useState(4)
+  const [confirmVisible, setConfirmVisible] = useState(false)
 
   const fetchEvent = async() => {
     try {
@@ -30,13 +32,16 @@ const ParticipantList = ({ eventID }: ParticipantListProps) => {
       setEvent(response.data)
       const numGroupTemp = Math.floor(response.data.teams.length / 3)
       setNumGroup(numGroupTemp)
-      setNumPlayoff(numGroupTemp * 2)
       setDraw(response.data.draw)
     }
     catch (error) {
       console.error('Error fetching event:', error)
     }
   }
+
+  useEffect(() => {
+    setNumPlayoff(numGroup * 2)
+  }, [numGroup])
 
   useEffect(() => {
     fetchEvent()
@@ -51,12 +56,15 @@ const ParticipantList = ({ eventID }: ParticipantListProps) => {
   }, [event])
 
   useEffect(() => {
-    if(draw.group?.length === 0 && event){
-      const initDisplayArray = event?.teams.map(() => -1)
+    if(!event || !draw.group)return
+
+    if(draw.group.length === 0){
+      const initDisplayArray = event.teams.map(() => -1)
       setGroupDisplayArray(initDisplayArray)
       return
     }
-    if(event && draw.group && draw.group?.length > 0){
+
+    if(draw.group.length > 0){
       const tempGroupDisplayArray = [...groupDisplayArray]
       for(let g = 0;g < draw.group.length;g++){
         for(let t = 0;t < draw.group[g].length;t++){
@@ -64,15 +72,46 @@ const ParticipantList = ({ eventID }: ParticipantListProps) => {
           tempGroupDisplayArray[idx] = g
         }
       }
+
+      for(let i = 0;i < event.teams.length - tempGroupDisplayArray.length;i++){
+        tempGroupDisplayArray.push(-1)
+      }
+
       setGroupDisplayArray(tempGroupDisplayArray)
     }
   }, [draw.group])
-  const onResetDraw = (eventID:string) => {
+
+  const onResetDraw = async(eventID:string) => {
     setDraw({})
     if(event){
       const initDisplayArray = event?.teams.map(() => -1)
       setGroupDisplayArray(initDisplayArray)
     }
+    await axios.put(`${SERVICE_ENDPOINT}/events/${eventID}`, {
+      draw: {}
+    }, { withCredentials:true })
+
+    setConfirmVisible(false)
+  }
+
+  const onSaveDraw = async(eventID:string) => {
+    try{
+      await axios.put(`${SERVICE_ENDPOINT}/events/${eventID}`, {
+        draw,
+      }, { withCredentials:true })
+
+    }catch (error){
+      if(axios.isAxiosError(error)){
+        console.error('Axios error:', error.message)
+        if (error.response) {
+          console.error('Status:', error.response.status)
+          console.error('Data:', error.response.data)
+        }
+      }else{
+        console.error('Unexpected error:', error)
+      }
+    }
+
   }
 
   const onRandomDraw = async(eventID: string) => {
@@ -95,6 +134,8 @@ const ParticipantList = ({ eventID }: ParticipantListProps) => {
       }
     }
   }
+
+
 
   const handleSelectChange = (group: (string|null|number), team: EventTeam, teamIndex: number) => {
     const oldGroup = groupDisplayArray[teamIndex]
@@ -122,7 +163,11 @@ const ParticipantList = ({ eventID }: ParticipantListProps) => {
     setShowPlayer(player)
     setAnchorEl(e.currentTarget)
   }
-  if(!event || groupDisplayArray.length !== event?.teams.length) return null
+
+  if(!event || groupDisplayArray.length === 0) {
+    return null
+  }
+
   return (
     <Box sx={{ display:'flex', gap: 1, overflow: 'hidden', p:1, flexDirection:'column'  }}>
       <Box sx={{ display:'flex', gap:2, margin: 1 }}>
@@ -141,72 +186,94 @@ const ParticipantList = ({ eventID }: ParticipantListProps) => {
           label="จำนวนทีมที่เข้ารอบ"
           variant="outlined"
           type='number' />
-        <Button sx={{ borderRadius: 10, width:'100px' }} onClick={() => onResetDraw(event.id)} color='error' variant='contained' size='large'>Reset</Button>
+        <Button sx={{ borderRadius: 10, width:'100px' }} onClick={() => setConfirmVisible(true)} color='error' variant='contained' size='large'>Reset</Button>
         <Button sx={{ borderRadius: 10, width:'100px'  }} onClick={() => onRandomDraw(event.id)} color='primary' variant='contained' size='large'>Random</Button>
-        <Button sx={{ borderRadius: 10, width:'100px'  }} color='primary' variant='contained' size='large'>Save</Button>
+        <Button sx={{ borderRadius: 10, width:'100px'  }} onClick={() => onSaveDraw(event.id)} color='primary' variant='contained' size='large'>Save</Button>
       </Box>
-      <TableContainer component={Paper} sx={{ maxWidth: '100%', maxHeight: 500 }} >
-        <Table stickyHeader size="small">
-          <TableHead>
-            <TableRow>
-              <TableCell>{t('tournament.participants.team')}</TableCell>
-              <TableCell>{t('tournament.participants.club')}</TableCell>
-              <TableCell>{'กลุ่ม'}</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {event.teams?.map((team, idx) => (
-              <TableRow key={idx}>
-                <TableCell>
-                  {team.players.map((player) => {
-                    return(<div key={player.id} onClick={(e) => handleShowPlayerDetail(e, player)}>
-                      <Typography >{player.officialName[language]}</Typography>
-                    </div>)
-                  })}
-                </TableCell>
-                <TableCell>
-                  {team.players.map((player) => <Typography key={player.id}>{player.club}</Typography>)}
-                </TableCell>
-                <TableCell>
-                  <FormControl fullWidth>
-                    <InputLabel id="event-label">{'Group'}</InputLabel>
-                    <Select
-                      size='small'
-                      required
-                      label="Event"
-                      labelId="event-label"
-                      value={groupDisplayArray[idx]?.toString()}
-                      onChange={(e) => handleSelectChange(e.target.value, team, idx)}>
-                      {MAP_GROUP_NAME.slice(0, numGroup).map((group) => (
-                        <MenuItem key={group.VALUE} value={group.VALUE}>
-                          {group.NAME}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </TableCell>
-
+      <Box sx={{ display:'flex' }}>
+        <TableContainer component={Paper} sx={{ maxWidth: '50%', maxHeight: 500 }} >
+          <Table stickyHeader size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell>{t('tournament.participants.team')}</TableCell>
+                <TableCell>{t('tournament.participants.club')}</TableCell>
+                <TableCell>{'กลุ่ม'}</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-        {showPlayer && <PlayerPopover showPlayer={showPlayer} setShowPlayer={setShowPlayer} anchorEl={anchorEl} setAnchorEl={setAnchorEl}/>}
-      </TableContainer>
-      {draw.group && draw.group.length > 0 && <Divider sx={{ mt:3, mb:3 }}/>}
-      <Box sx={{ display:'flex', flexWrap: 'wrap', gap:2, width:'100%', overflow:'scroll', p:1, pt:0 }}>
-        {
-          draw.group?.map((group, i) => <Card key={i} sx={{ width:'300px' }}>
-            <CardHeader title={MAP_GROUP_NAME[i].NAME} sx={{ borderBottom: '1px solid #ddd' }}/>
-            {
-              group?.map((team, j) => <CardContent key={j}>
-                {team?.players.map((p) => <Typography key={p.id}>{p.officialName[language]}</Typography>)}
-              </CardContent>)
-            }
-          </Card>)
-        }
+            </TableHead>
+            <TableBody>
+              {event.teams?.map((team, idx) => (
+                <TableRow key={idx}>
+                  <TableCell>
+                    {team.players.map((player) => {
+                      return(<div key={player.id} onClick={(e) => handleShowPlayerDetail(e, player)}>
+                        <Typography >{player.officialName[language]}</Typography>
+                      </div>)
+                    })}
+                  </TableCell>
+                  <TableCell>
+                    {team.players.map((player) => <Typography key={player.id}>{player.club}</Typography>)}
+                  </TableCell>
+                  <TableCell>
+                    <FormControl fullWidth>
+                      <InputLabel id="event-label">{'Group'}</InputLabel>
+                      <Select
+                        size='small'
+                        required
+                        label="Event"
+                        labelId="event-label"
+                        value={groupDisplayArray[idx]?.toString()}
+                        onChange={(e) => handleSelectChange(e.target.value, team, idx)}>
+                        {MAP_GROUP_NAME.slice(0, numGroup).map((group) => (
+                          <MenuItem key={group.VALUE} value={group.VALUE}>
+                            {group.NAME}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
+                  </TableCell>
+
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          {showPlayer && <PlayerPopover showPlayer={showPlayer} setShowPlayer={setShowPlayer} anchorEl={anchorEl} setAnchorEl={setAnchorEl}/>}
+        </TableContainer>
+        {draw.group && draw.group.length > 0 && <Divider sx={{ mt:3, mb:3 }}/>}
+        <Box sx={{ display:'flex', flexWrap: 'wrap', gap:2, width:'50%', overflow:'scroll', p:1, pt:0, maxHeight: 500 }}>
+          {
+            draw.group?.map((group, i) => <Card key={i} sx={{ width:'300px' }}>
+              <CardHeader title={MAP_GROUP_NAME[i].NAME} sx={{ borderBottom: '1px solid #ddd' }}/>
+              {
+                group?.map((team, j) => <CardContent key={j}>
+                  {team?.players.map((p) => <Typography key={p.id}>{p.officialName[language]}</Typography>)}
+                </CardContent>)
+              }
+            </Card>)
+          }
+        </Box>
       </Box>
       {draw.ko && draw.ko?.length > 0 && <Divider sx={{ mt:3, mb:3 }}/>}
-      <DrawBracket order={draw.ko ?? []}/>
+      <DrawBracket draw={draw} order={draw.ko ?? []} setDraw={setDraw}/>
+      <Dialog
+        open={confirmVisible}
+        slots={{
+          transition: Transition,
+        }}
+        keepMounted
+        onClose={() => setConfirmVisible(false)}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{'คุณแน่ใจที่จะลบสายการแข่งขันนี้หรือไม่?'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            ข้อมูลที่ได้ทำการกรอกไว้จะถูกลบออกหมด กรุณายืนยัน
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button color='error' onClick={() => onResetDraw(event.id)}>Confirm</Button>
+          <Button onClick={() => setConfirmVisible(false)}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   )
 }
--- src/app/tournaments/[id]/organizer/draw/drawBracket.tsx
+++ src/app/tournaments/[id]/organizer/draw/drawBracket.tsx
@@ -1,15 +1,50 @@
-import { EventTeam } from '@/type'
+import Transition from '@/app/components/ModalTransition'
+import { Event, EventTeam } from '@/type'
+import { Edit } from '@mui/icons-material'
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material'
+import { Dispatch, SetStateAction, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 const height = 50
 const lineWidth = 50
 
 interface Props {
+  draw: Event['draw']
   order: (EventTeam|string)[]
   blockWidth?: number
+  setDraw?: Dispatch<SetStateAction<Event['draw']>>
 }
 
-const DrawBracket = ({ order, blockWidth = 400 }: Props) => {
-
+const DrawBracket = ({ draw, order, blockWidth = 400, setDraw }: Props) => {
+  const [editModalVisible, setEditModalVisible] = useState(false)
+  const [selectedOrder, setSelectedOrder] = useState<number>(-1)
+  const [place, setPlace] = useState('')
+  const [group, setGroup] = useState('')
+  const { t } = useTranslation()
   const teamCount = order.length
+
+  const onClickEdit = (drawOrder: number) => {
+    setEditModalVisible(true)
+    setSelectedOrder(drawOrder)
+  }
+
+  const onCloseModal = () => {
+    setEditModalVisible(false)
+    setPlace('')
+    setGroup('')
+    setSelectedOrder(-1)
+  }
+
+  const onUpdateKODraw = () => {
+    const newOrderText = `ที่ ${place} กลุ่ม ${group}`
+    if(!draw.ko || selectedOrder < 0 || !setDraw) return
+    const tempDrawKO = [...draw.ko]
+    tempDrawKO[selectedOrder] = newOrderText
+    setDraw({ ...draw, ko: tempDrawKO })
+    onCloseModal()
+  }
+
+
+
 
   return (
     <>
@@ -26,20 +61,23 @@ const DrawBracket = ({ order, blockWidth = 400 }: Props) => {
                   borderRight: i % 2 === 1 ? '1px solid #333' : undefined,
                   display: 'flex',
                   alignItems: 'flex-end',
-                  gap: '10px'
+                  gap: '10px',
+                  justifyContent: 'space-between'
                 }}>
-                <div style={{ marginRight: '5px', width: '20px' }}>{i + 1}</div>
-                <div>{(typeof team === 'string') ? team : team.id}</div>
-                {/* <div><EditOutlined /></div> */}
+                <div style={{ display:'flex' }}>
+                  <div style={{ marginRight: '5px', width: '20px' }}>{i + 1}</div>
+                  <div>{(typeof team === 'string') ? team : team.id}</div>
+                </div>
+                {setDraw && <div style={{ marginRight: '10px' }} onClick={() => onClickEdit(i)}>< Edit/></div>}
               </div>
             )
           })}
         </div>
 
         {
-          Array.apply(null, Array(Math.ceil(Math.log2(teamCount || 1)))).map((round, i) => (
+          [... new Array(Math.ceil(Math.log2(teamCount || 1)))].map((round, i) => (
             <div key={`round-${i}`} style={{ paddingTop: `${(height / 2) + (i < 3 ? i : Math.pow(2, i - 1)) * height}px` }}>
-              {Array.apply(null, Array(teamCount - (i < 3 ? i : Math.pow(2, i - 1)))).map((team, j) => {
+              {[... new Array(teamCount - (i < 3 ? i : Math.pow(2, i - 1)))].map((team, j) => {
                 return <div
                   key={`key-${i * teamCount - (i < 3 ? i : Math.pow(2, i - 1)) + j}`}
                   style={{
@@ -55,6 +93,45 @@ const DrawBracket = ({ order, blockWidth = 400 }: Props) => {
 
 
       </div>
+      <Dialog
+        fullWidth
+        data-testid="note-modal"
+        open={editModalVisible}
+        onClose={onCloseModal}
+        slots={{ transition: Transition }}
+      >
+
+        <Box>
+          <DialogTitle sx={{ m: 0, p: 2 }} id="contact-person-dialog-title">
+            {t('tournament.draw.editKODraw')}
+          </DialogTitle>
+          <DialogContent dividers>
+            <TextField
+              value={place}
+              label={t('tournament.draw.placeInGroup')}
+              onChange={(e) => setPlace(e.target.value)}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              value={group}
+              label={t('tournament.draw.groupName')}
+              onChange={(e) => setGroup(e.target.value)}
+              fullWidth
+              margin="normal"
+            />
+
+          </DialogContent>
+          <DialogActions>
+            <Button variant='outlined' onClick={onCloseModal}>
+              {t('action.close')}
+            </Button>
+            <Button variant='contained' onClick={onUpdateKODraw}>
+              {t('action.save')}
+            </Button>
+          </DialogActions>
+        </Box>
+      </Dialog>
     </>
   )
 }
