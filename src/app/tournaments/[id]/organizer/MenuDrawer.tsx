7fc583610aa4bf6f56abc8e88daa05ad0111f6ae
Nonnadda <tontongfordev@gmail.com>
2026-05-31 12:54:32 +0300

    add collapsible side menu

--- src/app/tournaments/[id]/organizer/MenuDrawer.tsx
+++ src/app/tournaments/[id]/organizer/MenuDrawer.tsx
@@ -1,6 +1,11 @@
-import { Divider, Drawer, List, ListItem, ListItemButton, ListItemText, Toolbar } from '@mui/material'
+"use client"
 
-const drawerWidth = 200
+import { ChevronLeft, ChevronRight } from '@mui/icons-material'
+import { Box, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText, Toolbar, Tooltip } from '@mui/material'
+import { useState } from 'react'
+
+const expandedDrawerWidth = 200
+const collapsedDrawerWidth = 72
 
 const organizerMenu = [
   {
@@ -49,46 +54,70 @@ const duringTournamentMenu = [
 ]
 
 const MenuDrawer = ({ tournamentID }: {tournamentID: string}) => {
+  const [isCollapsed, setIsCollapsed] = useState(false)
+  const drawerWidth = isCollapsed ? collapsedDrawerWidth : expandedDrawerWidth
+
+  const renderMenuSection = (menus: { title: string; action: string }[]) => (
+    <List>
+      {menus.map((menu) => (
+        <ListItem key={menu.title} disablePadding>
+          <Tooltip title={isCollapsed ? menu.title : ''} placement="right">
+            <ListItemButton
+              href={`/tournaments/${tournamentID}/organizer/${menu.action}`}
+              sx={{
+                minHeight: 48,
+                justifyContent: isCollapsed ? 'center' : 'flex-start',
+                px: 2,
+              }}>
+              <ListItemText
+                primary={isCollapsed ? menu.title.slice(0, 1) : menu.title}
+                sx={{
+                  textAlign: isCollapsed ? 'center' : 'left',
+                  '& .MuiTypography-root': {
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    whiteSpace: 'nowrap',
+                    fontSize: isCollapsed ? '0.875rem' : '1rem',
+                  },
+                }} />
+            </ListItemButton>
+          </Tooltip>
+        </ListItem>
+      ))}
+    </List>
+  )
+
   return (<Drawer
     sx={{
       width: drawerWidth,
       flexShrink: 0,
-      ['& .MuiDrawer-paper']: { width: drawerWidth, boxSizing: 'border-box' },
+      transition: 'width 0.2s ease',
+      ['& .MuiDrawer-paper']: {
+        width: drawerWidth,
+        boxSizing: 'border-box',
+        overflowX: 'hidden',
+        transition: 'width 0.2s ease',
+        top: { xs: 56, md: 64 },
+        height: {
+          xs: 'calc(100vh - 56px)',
+          md: 'calc(100vh - 64px)',
+        },
+      },
     }}
     variant="permanent"
     anchor="left"
   >
-    <Toolbar />
+    <Toolbar sx={{ display: 'flex', justifyContent: isCollapsed ? 'center' : 'flex-end' }}>
+      <IconButton onClick={() => setIsCollapsed((prev) => !prev)} size="small">
+        {isCollapsed ? <ChevronRight /> : <ChevronLeft />}
+      </IconButton>
+    </Toolbar>
     <Divider />
-    <List>
-      {organizerMenu.map((menu) => (
-        <ListItem key={menu.title} disablePadding >
-          <ListItemButton href={`/tournaments/${tournamentID}/organizer/${menu.action}`}>
-            <ListItemText primary={menu.title} />
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
+    {renderMenuSection(organizerMenu)}
     <Divider />
-    <List>
-      {duringTournamentMenu.map((menu) => (
-        <ListItem key={menu.title} disablePadding >
-          <ListItemButton href={`/tournaments/${tournamentID}/organizer/${menu.action}`}>
-            <ListItemText primary={menu.title} />
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
+    {renderMenuSection(duringTournamentMenu)}
     <Divider />
-    <List>
-      {PersonelMenu.map((menu) => (
-        <ListItem key={menu.title} disablePadding >
-          <ListItemButton href={`/tournaments/${tournamentID}/organizer/${menu.action}`}>
-            <ListItemText primary={menu.title} />
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
+    {renderMenuSection(PersonelMenu)}
   </Drawer>)
 }
 export default MenuDrawer
